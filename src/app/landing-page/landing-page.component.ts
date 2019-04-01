3d7b873717545ba3301f6c7f13ace61edd0942a2
af39153 <conor.budge@anthem.com>
2019-04-01 16:48:13 -0500

    Added article and basic landing structure

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.components.scss']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
